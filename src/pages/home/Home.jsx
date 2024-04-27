e4480e865b8c861e3cf79fff2dd43a10d3dd51b5
Jasur0919 <jasurjohn1111@gmail.com>
2024-04-27 10:03:56 +0500

    inter-card

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Home.css'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
 import Swiper1 from '../../assets/Swipper.jpg'
+import Inter1 from '../../assets/inter-1.png'
+import Inter2 from '../../assets/inter-2.png'
+import Inter3 from '../../assets/inter-3.png'
+import Inner1 from '../../assets/inner-1.png'
 
+
+const data = [
+  {
+    id: 1,
+    img: Inter1,
+    title: "Everyday Fresh & Clean with Our Products",
+    tog: "shop Now"
+  },
+  {
+    id: 2,
+    img: Inter2,
+    title: "Make your Breakfast Healthy and Easy",
+    tog: "shop Now"
+  },
+  {
+    id: 3,
+    img: Inter3,
+    title: "The best Organic Products Online",
+    tog: "shop Now"
+  }
+]
 
 const Home = () => {
+
+let cards = data?.map((el) => (
+  <div key={el.id} className="inter_card">
+              <img  src={el.img} alt="" />
+              <div className="inter_card_text">
+                <h3>{el.title}</h3>
+                <button>Shop Now</button>
+              </div>
+        </div>
+))
+
   return (
     <>
 
     
       <section className="logo">
-        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <Swiper loop={true} navigation={true} modules={[Navigation]} className="mySwiper">
               <SwiperSlide className='logo_swiper'>
                 <img src={Swiper1} alt="" />
                 <div className="logo_text">
@@ -57,6 +93,77 @@ const Home = () => {
               <SwiperSlide>Slide 8</SwiperSlide>
               <SwiperSlide>Slide 9</SwiperSlide> */}
             </Swiper>
+  </section>
+
+  <section className="inter">
+    <div className="container">
+      <div className="inter_box">
+        {cards}
+        {/* <div className="inter_card">
+              <img  src={Inter1} alt="" />
+              <div className="inter_card_text">
+                <h3>Everyday Fresh & Clean with Our Products</h3>
+                <button>Shop Now</button>
+              </div>
+        </div>
+        <div className="inter_card">
+              <img  src={Inter1} alt="" />
+              <div className="inter_card_text">
+                <h3>Everyday Fresh & Clean with Our Products</h3>
+                <button>Shop Now</button>
+              </div>
+        </div>
+        <div className="inter_card">
+              <img  src={Inter1} alt="" />
+              <div className="inter_card_text">
+                <h3>Everyday Fresh & Clean with Our Products</h3>
+                <button>Shop Now</button>
+              </div>
+        </div> */}
+      </div>
+    </div>
+  </section>
+
+  <section className="inner">
+    <div className="container">
+      <div className="inner_box">
+        <div className="inner_card">
+          <img src={Inner1} alt="" />
+          <div className="inner_text">
+            <h3>Best prices & offers</h3>
+            <p>Orders $50 or more</p>
+          </div>
+        </div>
+        <div className="inner_card">
+          <img src={Inner1} alt="" />
+          <div className="inner_text">
+            <h3>Best prices & offers</h3>
+            <p>Orders $50 or more</p>
+          </div>
+        </div>
+        <div className="inner_card">
+          <img src={Inner1} alt="" />
+          <div className="inner_text">
+            <h3>Best prices & offers</h3>
+            <p>Orders $50 or more</p>
+          </div>
+        </div>
+        <div className="inner_card">
+          <img src={Inner1} alt="" />
+          <div className="inner_text">
+            <h3>Best prices & offers</h3>
+            <p>Orders $50 or more</p>
+          </div>
+        </div>
+        <div className="inner_card">
+          <img src={Inner1} alt="" />
+          <div className="inner_text">
+            <h3>Best prices & offers</h3>
+            <p>Orders $50 or more</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 
     </>
